0750293d2a747d988c765d3d31eb3cf0657bd15d
William Rothman <wdrothman@gmail.com>
2023-09-26 21:03:05 +0200

    feat(website): add default layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "../app.postcss";
+  import "../app.css";
+  import Header from "$lib/components/Header.svelte";
+</script>
+
+<div>
+  <Header />
+
+  <main>
+    <slot />
+  </main>
+</div>
