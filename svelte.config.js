b3370586fcb37a57f679a9ce7bb5ef442a97aaa6
William Rothman <wdrothman@gmail.com>
2023-09-26 21:02:37 +0200

    feat(svelte): add vitepreprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import { vitePreprocess } from "@sveltejs/kit/vite";
 import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -5,9 +6,11 @@ const config = {
   kit: {
     adapter: adapter(),
     paths: {
-      base: process.env.name === "github-pages" ? "/sveltekit-gh-pages" : ""
+      base: process.env.name === "github-pages" ? "/sveltekit-gh-pages" : "",
     },
   },
+
+  preprocess: [vitePreprocess({})],
 };
 
 export default config;
