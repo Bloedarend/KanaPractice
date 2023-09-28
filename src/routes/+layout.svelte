ac86030993cb296c7ee7f2ade8403d8f655fd4fd
William Rothman <wdrothman@gmail.com>
2023-09-28 09:49:15 +0200

    feat(layout): use data for head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,25 @@
-<script>
-  import "../app.postcss";
-  import "../app.css";
-  import Header from "$lib/components/Header.svelte";
+<script lang="ts">
+    import "../app.postcss";
+    import "../app.css";
+    import Header from "$lib/components/Header.svelte";
+    import {page} from "$app/stores";
+
+    export let data;
+
+    $: activeUrl = $page.url.pathname;
+    $: activeName = (activeUrl !== "/" ? activeUrl.substring(activeUrl.slice("/") + 1) : "home")
+    $: activeNameFormatted = activeName.charAt(0).toUpperCase() + activeName.slice(1);
 </script>
 
-<div>
-  <Header />
+<svelte:head>
+    <title>{data.name} • {activeNameFormatted}</title>
+    <link rel="icon" type="image/x-icon" href="{data.logo}">
+</svelte:head>
 
-  <main>
-    <slot />
-  </main>
+<div>
+<!--    <Header {data} />-->
+
+    <main>
+        <slot />
+    </main>
 </div>
