890c8ff28de9e150abce75ecc7837d93c524e492
William Rothman <60738365+Bloedarend@users.noreply.github.com>
2023-10-02 11:57:23 +0200

    feat(website): add layout for error page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,12 @@
 <script lang="ts">
     import "../app.postcss";
-    import "../app.css";
-    import Header from "$lib/components/Header.svelte";
-    import {page} from "$app/stores";
 
     export let data;
-
-    $: activeUrl = $page.url.pathname;
-    $: activeName = (activeUrl !== "/" ? activeUrl.substring(activeUrl.slice("/") + 1) : "home")
-    $: activeNameFormatted = activeName.charAt(0).toUpperCase() + activeName.slice(1);
 </script>
 
 <svelte:head>
-    <title>{data.name} • {activeNameFormatted}</title>
+    <title>{data.name}</title>
     <link rel="icon" type="image/x-icon" href="{data.logo}">
 </svelte:head>
 
-<div>
-<!--    <Header {data} />-->
-
-    <main>
-        <slot />
-    </main>
-</div>
+<slot />
