e79f5508b52234a87b5d2ce16eb611058b5b686a
William Rothman <60738365+Bloedarend@users.noreply.github.com>
2023-10-02 11:56:51 +0200

    feat(website): add error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,7 +1,16 @@
-<script>
+<script lang="ts">
     import { page } from '$app/stores';
+    import {base} from "$app/paths";
+
+    export let data;
 </script>
 
-<div class="bg-[#1da1f2]">
-    <h1>{$page.status}: {$page.error.message}</h1>
+<div class="flex flex-row items-center justify-center absolute w-full h-full">
+    <a href="{base}/">
+        <img class="h-60 w-60" src="{data.logo}" alt="Logo" />
+    </a>
+    <div class="flex flex-col m-10 h-60 w-60 items-center justify-center text-center">
+        <p class="text-9xl">{$page.status}</p>
+        <p class="text-3xl break-all">{$page.error.message}</p>
+    </div>
 </div>
