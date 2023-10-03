051f0031d06959638657beb90251c40ea2ac01f8
William Rothman <60738365+Bloedarend@users.noreply.github.com>
2023-10-03 10:08:08 +0200

    feat(navlist): make navlist appear when clicking hamburger

--- src/lib/components/navigation/NavList.svelte
+++ src/lib/components/navigation/NavList.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-    export let visible: Boolean = false;
-    let classes = "portrait:hidden"
+    import { navigationState } from "$lib/stores/session-store";
 
-    let toggleVisibility = (visibility) => {
-        visible = visibility;
-        classes = visible ? "portrait:flex-column portrait:absolute" : "portrait: hidden";
-    }
+    const visibleClasses: string = "portrait:flex-column portrait:absolute portrait:top-0 portrait:bottom-0 portrait:left-0 portrait:bg-primary-300";
+    const hiddenClasses: string = "portrait:hidden";
 </script>
 
-<div class="flex items-center {classes}">
+<div class="flex items-center {$navigationState.listIsVisible ? visibleClasses : hiddenClasses}">
     <slot/>
 </div>
