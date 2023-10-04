9c474ce40faee784c17751e86263d21f697aa2a1
William Rothman <wdrothman@gmail.com>
2023-10-04 14:40:34 +0200

    fix(svelteconfig): change ts to js to fix weird issue

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,14 @@
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import adapter from "@sveltejs/adapter-static";
-import { Config } from "@sveltejs/kit";
 
-const config: Config = {
+const config = {
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
     paths: {
       base: process.env.name === "github-pages" ? "/sveltekit-gh-pages" : "",
     },
   },
-
-  preprocess: vitePreprocess(),
 };
 
 export default config;
