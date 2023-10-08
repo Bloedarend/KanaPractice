2d63157dbaaf8b3b2611f11fc1581518b277728e
William Rothman <wdrothman@gmail.com>
2023-10-08 23:09:25 +0200

    fix(navbar): fix content being hidden under navbar

--- src/lib/components/navigation/NavBar.svelte
+++ src/lib/components/navigation/NavBar.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-     export let className: string = "h-12 p-2 shadow-lg";
+     export let className: string = "bg-surface-50 dark:bg-surface-900 h-12 p-2 shadow-lg";
 </script>
 
-<nav class="{className} flex sticky left-0 right-0 top-0 w-full">
+<div class="{className}"></div> <!-- Empty div to make content start below the fixed navbar -->
+<nav class="{className} flex fixed top-0 z-100 w-full">
     <slot />
 </nav>
