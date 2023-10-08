801ce044dc3942cab307cc92ec7375c135c72513
William Rothman <wdrothman@gmail.com>
2023-10-08 21:36:54 +0200

    feat(home): temporarily add more text to see scrollbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
 <div>yo</div>
 <div>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br></div>
+
+<div>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br></div>
+
+<div>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br>hello<br></div>
