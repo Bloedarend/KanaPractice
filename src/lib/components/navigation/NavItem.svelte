c2d49bb69dd427817537b802fbd8ca31be2992f7
William Rothman <wdrothman@gmail.com>
2023-10-08 21:35:36 +0200

    fix(navitem): fix home icon never being active

--- src/lib/components/navigation/NavItem.svelte
+++ src/lib/components/navigation/NavItem.svelte
@@ -7,11 +7,13 @@
     href = href ?? `${base}/`;
     activeUrl = activeUrl ?? "";
 
+    console.log(href, activeUrl, `${base}/`);
+
     export let className: string = "flex flex-row items-center mx-3 mb-0.5 my-0 text-md hover:text-secondary-500 dark:hover:text-primary-500 dark:fill-primary-50 hover:fill-secondary-500 dark:hover:fill-primary-500";
     export let activeClassName: string = "flex flex-row items-center mx-3 mb-0.5 my-0 text-md text-secondary-500 dark:text-primary-500 fill-secondary-500 dark:fill-primary-500";
 </script>
 
-<a class="{!activeUrl.includes(href) || href === base + '/' ? className : activeClassName}" href="{href}">
+<a class="{activeUrl.startsWith(href) && href !== `${base}/` || href === activeUrl ? activeClassName : className}" href="{href}">
     <slot name="icon" />
     <slot name="name" />
 </a>
