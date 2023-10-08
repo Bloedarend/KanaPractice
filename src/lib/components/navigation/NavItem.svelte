2f298ca2d23afa2761e09463250c718ad2c290d2
William Rothman <wdrothman@gmail.com>
2023-10-08 23:09:44 +0200

    refactor(navitem): remove debug logging

--- src/lib/components/navigation/NavItem.svelte
+++ src/lib/components/navigation/NavItem.svelte
@@ -7,8 +7,6 @@
     href = href ?? `${base}/`;
     activeUrl = activeUrl ?? "";
 
-    console.log(href, activeUrl, `${base}/`);
-
     export let className: string = "flex flex-row items-center mx-3 mb-0.5 my-0 text-md hover:text-secondary-500 dark:hover:text-primary-500 dark:fill-primary-50 hover:fill-secondary-500 dark:hover:fill-primary-500";
     export let activeClassName: string = "flex flex-row items-center mx-3 mb-0.5 my-0 text-md text-secondary-500 dark:text-primary-500 fill-secondary-500 dark:fill-primary-500";
 </script>
