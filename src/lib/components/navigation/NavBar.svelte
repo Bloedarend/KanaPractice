8b816f6af45ee5549bd84ffccef3ea51f875a19f
William Rothman <wdrothman@gmail.com>
2023-10-08 21:36:18 +0200

    feat(navbar): add shadow

--- src/lib/components/navigation/NavBar.svelte
+++ src/lib/components/navigation/NavBar.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-     export let className: string = "bg-surface-50 dark:bg-surface-800 h-12 p-2";
+     export let className: string = "h-12 p-2 shadow-lg";
 </script>
 
-<div class="{className}"></div> <!-- Empty div to take up same space as navbar -->
-<nav class="{className} flex absolute left-0 right-0 top-0 w-full">
+<nav class="{className} flex sticky left-0 right-0 top-0 w-full">
     <slot />
 </nav>
