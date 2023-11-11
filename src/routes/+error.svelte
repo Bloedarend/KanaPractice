f6f7ef4dc3c254db36899ccde0c95cae2a41fe5b
William Rothman <wdrothman@gmail.com>
2023-11-11 01:12:15 +0100

    refactor(error): change styling

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
     import { page } from '$app/stores';
-    import { base } from "$app/paths";
-    import { PUBLIC_LOGO } from '$env/static/public'
+    import { PUBLIC_NAME, PUBLIC_REPO_FRONTEND } from "$env/static/public";
 
-    const error: string = $page.error !== null ? $page.error.message : "Unexpected error."
+    const code: string = $page.status ?? "000";
+    const message: string = $page.error !== null ? $page.error.message : "Unknown";
 </script>
 
-<div class="flex flex-row items-center justify-center absolute w-full h-full">
-    <a href="{base}/">
-        <img class="h-60 w-60" src="{PUBLIC_LOGO}" alt="Logo" />
-    </a>
-    <div class="flex flex-col m-10 h-60 w-60 items-center justify-center text-center">
-        <p class="text-9xl">{$page.status}</p>
-        <p class="text-3xl break-all">{error}</p>
+<svelte:head>
+    <title>{PUBLIC_NAME} • {$page.status}</title>
+</svelte:head>
+
+<div class="flex flex-row justify-center items-center">
+    <div class="flex flex-col w-96 p-10 items-center justify-center">
+        <p class="text-3xl text-center">{code} | {message}</p>
+        <div class="absolute bottom-0 items-center justify-center">
+            <p>If this is a bug, please consider opening an issue on the <a class="text-primary-500" href="{PUBLIC_REPO_FRONTEND}/issues">GitHub page</a>. Click the buttons below to either go back to the previous page or to go to the home page.</p>
+            <button class="btn variant-filled-primary">Back</button>
+        </div>
     </div>
 </div>
