22ce8189a82ae403fc548f1aad9934c81c5d1efb
William Rothman <wdrothman@gmail.com>
2023-11-13 00:34:55 +0100

    refactor(error): change page styling

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { page } from '$app/stores';
     import { PUBLIC_NAME } from "$env/static/public";
-    import Button from "$lib/components/interaction/Button.svelte";
 
     const code: string = $page.status ?? "000";
     const message: string = $page.error !== null ? $page.error.message : "Unknown";
@@ -11,11 +10,9 @@
     <title>{PUBLIC_NAME} • {$page.status}</title>
 </svelte:head>
 
-<div class="flex flex-col justify-center items-center h-full">
-    <div class="flex flex-grow justify-center items-center">
-        <Button className="text-3xl text-center p-6 mb-48 rounded-xl bg-surface-200-700-token shadow-inner">{code} | {message}</Button>
-    </div>
-    <div class="flex flex-col justify-center items-center p-2 rounded-t-3xl w-full shadow-t-md">
-        <a class="m-2 text-center" href="/issues">If you believe this to be a bug, please consider opening an issue on the Github repository.</a>
+<div class="flex flex-col h-full">
+    <div class="flex flex-col flex-grow justify-center items-center pb-10">
+        <div class="text-4xl text-center p-3 rounded-xl shadow-inner">{code} | {message}</div>
+        <a class="m-2 text-center w-2/3" href="/issues">If you believe this to be a bug, please consider opening an issue on the Github repository.</a>
     </div>
 </div>
