bd3ab005006ce53a9ca6ae1e7b6483a984019006
William Rothman <wdrothman@gmail.com>
2023-11-13 17:10:52 +0100

    feat(error): add redirect to github repo

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { page } from '$app/stores';
-    import { PUBLIC_NAME } from "$env/static/public";
+    import { PUBLIC_NAME, PUBLIC_GITHUB_REPO } from "$env/static/public";
 
     const code: string = $page.status ?? "000";
     const message: string = $page.error !== null ? $page.error.message : "Unknown";
@@ -13,6 +13,6 @@
 <div class="flex flex-col h-full">
     <div class="flex flex-col flex-grow justify-center items-center pb-10">
         <div class="text-4xl text-center p-3 rounded-xl shadow-inner">{code} | {message}</div>
-        <a class="m-2 text-center w-2/3" href="/issues">If you believe this to be a bug, please consider opening an issue on the Github repository.</a>
+        <a class="m-2 text-center w-2/3" target="_blank" href="{PUBLIC_GITHUB_REPO}/issues">If you believe this to be a bug, please consider opening an issue on the Github repository.</a>
     </div>
 </div>
